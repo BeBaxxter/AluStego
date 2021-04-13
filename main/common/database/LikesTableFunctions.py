050683a9c400383808ac0a6e48a495508be26c32
DESKTOP-VFUM3QM\Robin <robinwettstadt@gmail.com>
2021-04-13 19:28:05 +0200

    First database files commit try

---
+++ main/common/database/LikesTableFunctions.py
@@ -0,0 +1,248 @@
+import PostsTableFunctions
+import UsersTableFunctions
+from CommentsTableFunctions import *
+from UsersTableFunctions import *
+
+
+# Insert Functions:
+
+def insert_new_like(values):
+    """ inserts a new like into the database
+    :param values: 1.: like_post_id, 2.: like_user_id
+    :return: None
+    """
+    try:
+        conn = create_connection()
+        c = conn.cursor()
+        post_exists_check = PostsTableFunctions.check_if_post_exists(values[0])
+        if post_exists_check == values[0]:
+            like_exists_check = check_if_like_exists(values)
+            if like_exists_check != values:
+                c.execute("""
+                    INSERT INTO likes (like_post_id, like_user_id) 
+                    VALUES (?, ?);""", values)
+
+                post_id = values[0]
+                current_number_of_likes = PostsTableFunctions.get_post_number_of_likes(post_id)
+                if current_number_of_likes is not None:
+                    new_number_of_likes = current_number_of_likes + 1
+                    c.execute(f"""UPDATE posts
+                                          SET number_of_likes = ?
+                                          WHERE post_id = ?""", (new_number_of_likes, values[0],))
+
+        conn.commit()
+        conn.close()
+    except Error as e:
+        print(e)
+        conn.rollback()
+
+
+# Delete Functions:
+
+def delete_like(values):
+    """ deletes a given like
+    :param values: like_post_id, like_user_id
+    :return: None
+    """
+    try:
+        like_id = get_like_id(values)
+        post_id = get_like_post_id(like_id)
+        if post_id:
+            current_number_of_likes = PostsTableFunctions.get_post_number_of_likes(post_id)
+            if current_number_of_likes is not None:
+                new_number_of_likes = current_number_of_likes - 1
+                conn = create_connection()
+                c = conn.cursor()
+                c.execute(f"""UPDATE posts
+                              SET number_of_likes = ?
+                              WHERE post_id = ?""", (new_number_of_likes, post_id))
+
+                c.execute(f"DELETE FROM likes WHERE like_id = ?;", (like_id,))
+
+                conn.commit()
+                conn.close()
+    except Error as e:
+        print(e)
+        conn.rollback()
+
+
+def delete_all_likes_of_user(user_name):
+    """ deletes all likes of a given user
+    :param user_name: username of the user who's likes shall be deleted
+    :return: None
+    """
+    try:
+        conn = create_connection()
+        cur = conn.cursor()
+
+        user_id = UsersTableFunctions.get_user_id(user_name)
+        if user_id is not None:
+            posts = get_all_post_ids_of_users_likes(user_name)
+            if posts is not None:
+                for post in posts:
+                    current_number_of_likes = PostsTableFunctions.get_post_number_of_likes(post)
+                    new_number_of_likes = current_number_of_likes - 1
+
+                    cur.execute(f"""UPDATE posts
+                                            SET number_of_likes = ?
+                                            WHERE post_id = ?""", (new_number_of_likes, post))
+
+                cur.execute(f"DELETE FROM likes WHERE like_user_id = ?;", (user_id,))
+
+        conn.commit()
+        conn.close()
+    except Error as e:
+        print(e)
+        conn.rollback()
+
+
+# Select Functions:
+
+def get_like_id(values):
+    """ returns like_id of a given like
+    :param values: 1.: like_post_id, 2.: like_user_id
+    :return: like_id (Integer in Tuple)
+    """
+    try:
+        conn = create_connection()
+        cur = conn.cursor()
+        cur.execute(f"""
+                        SELECT like_id FROM likes
+                        WHERE like_post_id = ? AND like_user_id = ?;""", values)
+        row = cur.fetchone()
+        conn.commit()
+        conn.close()
+        if row:
+            return row[0]
+        else:
+            return None
+    except Error as e:
+        print(e)
+        conn.rollback()
+
+
+def get_like_post_id(like_id):
+    """ returns like_post_id of a given like
+    :param like_id: ID of the given Like (Integer)
+    :return: like_post_id (Integer in Tuple)
+    """
+    try:
+        conn = create_connection()
+        cur = conn.cursor()
+        cur.execute(f"""
+                        SELECT like_post_id FROM likes
+                        WHERE like_id = ? ;""", (like_id, ))
+        row = cur.fetchone()
+        conn.commit()
+        conn.close()
+        if row:
+            return row[0]
+        else:
+            return None
+    except Error as e:
+        print(e)
+        conn.rollback()
+
+
+def get_like_user_id(like_id):
+    """ returns like_user_id of a given like
+    :param like_id: ID of the given Like (Integer)
+    :return: like_user_id (Integer in Tuple)
+    """
+    try:
+        conn = create_connection()
+        cur = conn.cursor()
+        cur.execute(f"""
+                        SELECT like_user_id FROM likes
+                        WHERE like_id = ? ;""", like_id)
+        row = cur.fetchone()
+        conn.commit()
+        conn.close()
+        if row:
+            return row[0]
+        else:
+            return None
+    except Error as e:
+        print(e)
+        conn.rollback()
+
+
+def get_all_likes_of_user(user_id):
+    """ returns all like_id's of a given user
+    :param user_id: ID of the user
+    :return: Tuple of like_id's (Integer)
+    """
+    try:
+        conn = create_connection()
+        cur = conn.cursor()
+        cur.execute(f"""
+                        SELECT like_id FROM likes
+                        WHERE  like_user_id = ?;""", (user_id, ))
+        row = cur.fetchall()
+        conn.commit()
+        conn.close()
+        if row:
+            tuple_of_like_ids = ()
+            for like_id in row:
+                tuple_of_like_ids = tuple_of_like_ids + like_id
+            if tuple_of_like_ids:
+                return tuple_of_like_ids
+        else:
+            return None
+    except Error as e:
+        print(e)
+        conn.rollback()
+
+
+def get_all_post_ids_of_users_likes(user_name):
+    """ returns all like_post_id's of a given user
+    :param user_name: username of the user
+    :return: Tuple of like_post_id's (Integer)
+    """
+    try:
+        conn = create_connection()
+        cur = conn.cursor()
+        user_id = UsersTableFunctions.get_user_id(user_name)
+        cur.execute(f"""
+                        SELECT like_post_id FROM likes
+                        WHERE  like_user_id = ?;""", (user_id, ))
+        row = cur.fetchall()
+        conn.commit()
+        conn.close()
+        if row:
+            tuple_of_like_ids = ()
+            for like_id in row:
+                tuple_of_like_ids = tuple_of_like_ids + like_id
+            if tuple_of_like_ids:
+                return tuple_of_like_ids
+        else:
+            return None
+    except Error as e:
+        print(e)
+        conn.rollback()
+
+
+# Checking Functions:
+
+def check_if_like_exists(values):
+    """ checks if a like on a given post by a given user already exists
+        (checks if the given user has already liked the given post)
+    :param values: like_post_id, like_user_id
+    :return: values (like_post_id, like_user_id)
+    """
+    try:
+        conn = create_connection()
+        cur = conn.cursor()
+        cur.execute(f"""
+                        SELECT like_post_id, like_user_id FROM likes
+                        WHERE  like_post_id = ? AND like_user_id = ?;""", values)
+        row = cur.fetchone()
+        conn.commit()
+        conn.close()
+        if row:
+            return row
+        else:
+            return None
+    except Error as e:
+        print(e)
+        conn.rollback()
---
+++ main/common/database/PostsTableFunctions.py
@@ -0,0 +1,361 @@
+import sqlite3
+from sqlite3 import Error
+import FriendsTableFunctions
+import UsersTableFunctions
+from TableCreationFunctions import *
+from UsersTableFunctions import *
+from FriendsTableFunctions import *
+from CommentsTableFunctions import *
+from LikesTableFunctions import *
+
+
+# Insert Functions:
+
+def insert_new_post(values):
+    """ inserts a new post into the database
+    :param values: 0.: author_name, 1.: og_author_name, 2.: date_time,
+                   3.: date_time, 4.: content, 5.: type_of_post,
+                   6.: number_of_likes
+    return: None
+    """
+    try:
+        author_id = UsersTableFunctions.get_user_id(values[0])
+        og_author_id = UsersTableFunctions.get_user_id(values[1])
+        conn = create_connection()
+        c = conn.cursor()
+        c.execute("""
+            INSERT INTO posts (author_id, og_author_id, date_time, content, type_of_post, number_of_likes) 
+            VALUES (?, ?, ?, ?, ?, 0);""", (author_id, og_author_id, values[2], values[3], values[4], ))
+        conn.commit()
+        conn.close()
+    except Error as e:
+        print(e)
+
+
+# Delete Functions:
+
+def delete_post(post_id):
+    """ deletes a given post including likes and comments
+    :param post_id: post_id
+    :return: None
+    """
+    try:
+        delete_all_comments_of_post(post_id)
+        conn = create_connection()
+        cur = conn.cursor()
+        cur.execute(f"DELETE FROM likes WHERE like_post_id = ?;", (post_id, ))
+        cur.execute(f"DELETE FROM posts WHERE post_id=?;", (post_id, ))
+        conn.commit()
+        conn.close()
+    except Error as e:
+        print(e)
+        conn.rollback()
+
+
+def delete_all_posts_of_user(user_id):
+    """ deletes all posts of a given user including likes and comments
+    :param user_id: ID of the user who's posts shall be deleted
+    :return: None
+    """
+    try:
+        conn = create_connection()
+        cur = conn.cursor()
+        post_id_tuple = get_all_posts_of_user(user_id)
+        if post_id_tuple is not None:
+            for post in post_id_tuple:
+                cur.execute(f"DELETE FROM likes WHERE like_post_id = ?;", (post, ))
+
+        cur.execute(f"DELETE FROM posts WHERE author_id = ? OR og_author_id = ?;", (user_id, user_id, ))
+        conn.commit()
+        conn.close()
+    except Error as e:
+        print(e)
+        conn.rollback()
+
+
+# seemingly not needed
+# def delete_all_likes_on_posts_of_user(user_id):
+#     """ deletes all likes on posts of a given user
+#     :param user_id:
+#     :return: None
+#     """
+#     try:
+#         conn = create_connection()
+#         cur = conn.cursor()
+#         post_id_tuple = get_all_posts_of_user(user_id)
+#         if post_id_tuple is not None:
+#             for post in post_id_tuple:
+#                 cur.execute(f"DELETE FROM likes WHERE like_post_id = ?;", (post, ))
+#         conn.commit()
+#         conn.close()
+#     except Error as e:
+#         print(e)
+#         conn.rollback()
+
+
+# Select Functions:
+
+def get_news_feed_post_ids(user_name):
+    """ returns the post_ids of the posts of all friends of a given user to display on the home feed
+        :param user_name
+        :return: post_id's in a Tuple
+        """
+    try:
+        user_id = UsersTableFunctions.get_user_id(user_name)
+        friends_of_user = FriendsTableFunctions.get_all_friends_ids(user_id)
+        list_of_post_ids = []
+        conn = create_connection()
+        cur = conn.cursor()
+        if friends_of_user is not None:
+            for friend in friends_of_user:
+                cur.execute(f"""
+                                    SELECT post_id FROM posts
+                                    WHERE author_id = ?;""", (friend,))
+                row = cur.fetchall()
+                if row:
+                    list_of_post_ids.append(row)
+            conn.commit()
+            conn.close()
+            if row:
+                tuple_of_post_ids = ()
+                for post in list_of_post_ids[0]:
+                    tuple_of_post_ids = tuple_of_post_ids + post
+                if tuple_of_post_ids:
+                    return tuple_of_post_ids
+                else:
+                    return None
+        else:
+            return None
+    except Error as e:
+        print(e)
+        conn.rollback()
+
+
+def get_all_posts_of_user(user_name):
+    """ returns posts of a given user
+        :param user_name
+        :return: Tuple with all post_id's of that user
+        """
+    try:
+        user_id = UsersTableFunctions.get_user_id(user_name)
+        conn = create_connection()
+        cur = conn.cursor()
+        cur.execute(f"""
+                            SELECT post_id FROM posts
+                            WHERE author_id = ? OR og_author_id = ?;""", (user_id, user_id, ))
+        row = cur.fetchall()
+        conn.commit()
+        conn.close()
+        if row:
+            posts_tuple = ()
+            for post in row:
+                posts_tuple = posts_tuple + post
+            print(posts_tuple)
+            return posts_tuple
+        else:
+            return None
+    except Error as e:
+        print(e)
+        conn.rollback()
+
+
+def get_post_number_of_likes(post_id):
+    """ returns the number of likes a given post has
+    :param post_id: post_id
+    :return: number_of_likes (Integer)
+    """
+    try:
+        conn = create_connection()
+        cur = conn.cursor()
+        cur.execute(f"""
+                        SELECT number_of_likes FROM posts
+                        WHERE post_id = ?;""", (post_id, ))
+        row = cur.fetchone()
+        conn.commit()
+        conn.close()
+        if row:
+            return row[0]
+        else:
+            return None
+    except Error as e:
+        print(e)
+        conn.rollback()
+
+
+def get_post_id(values):
+    """ returns post_id of a given post
+    :param values: author_id, og_author_id, date_time, content, type_of_post
+    :return: post_id (Integer)
+    """
+    try:
+        author_id = UsersTableFunctions.get_user_id(values[0])
+        og_author_id = UsersTableFunctions.get_user_id(values[1])
+        conn = create_connection()
+        cur = conn.cursor()
+        cur.execute(f"""
+                        SELECT post_id FROM posts
+                        WHERE author_id = ? 
+                        AND og_author_id = ? 
+                        AND date_time = ?
+                        AND content = ?
+                        AND type_of_post = ?;""", (author_id, og_author_id, values[2], values[3], values[4], ))
+        row = cur.fetchone()
+        conn.commit()
+        conn.close()
+        if row:
+            return row[0]
+        else:
+            return None
+    except Error as e:
+        print(e)
+        conn.rollback()
+
+
+def get_post_author_id(post_id):
+    """ returns author_id of a given post
+    :param post_id: post_id
+    :return: author_id (Integer)
+    """
+    try:
+        conn = create_connection()
+        cur = conn.cursor()
+        cur.execute(f"""
+                        SELECT author_id FROM posts
+                        WHERE post_id = ?;""", (post_id, ))
+        row = cur.fetchone()
+        conn.commit()
+        conn.close()
+        if row:
+            return row[0]
+        else:
+            return None
+    except Error as e:
+        print(e)
+        conn.rollback()
+
+
+def get_post_og_author_id(post_id):
+    """ returns og_author_id of a given post
+    :param post_id: post_id
+    :return: og_author_id (Integer)
+    """
+    try:
+        conn = create_connection()
+        cur = conn.cursor()
+        cur.execute(f"""
+                        SELECT og_author_id FROM posts
+                        WHERE post_id = ?;""", (post_id, ))
+        row = cur.fetchone()
+        conn.commit()
+        conn.close()
+        if row:
+            return row[0]
+        else:
+            return None
+    except Error as e:
+        print(e)
+        conn.rollback()
+
+
+def get_post_date_time(post_id):
+    """ returns date_time of a given post
+    :param post_id: post_id
+    :return: date_time (String)
+    """
+    try:
+        conn = create_connection()
+        cur = conn.cursor()
+        cur.execute(f"""
+                        SELECT date_time FROM posts
+                        WHERE post_id = ?;""", (post_id, ))
+        row = cur.fetchone()
+        conn.commit()
+        conn.close()
+        if row:
+            return row[0]
+        else:
+            return None
+    except Error as e:
+        print(e)
+        conn.rollback()
+
+
+def get_post_content(post_id):
+    """ returns the content of a given post
+    :param post_id: post_id
+    :return: content of the post / may be a link to the picture or video on the server (String)
+    """
+    try:
+        conn = create_connection()
+        cur = conn.cursor()
+        cur.execute(f"""
+                        SELECT content FROM posts
+                        WHERE post_id = ?;""", (post_id, ))
+        row = cur.fetchone()
+        conn.commit()
+        conn.close()
+        if row:
+            return row[0]
+        else:
+            return None
+    except Error as e:
+        print(e)
+        conn.rollback()
+
+
+def get_type_of_post(post_id):
+    """ returns the type of a given post
+        1 = content in the form of Text
+        2 = content in the form of Picture
+        3 = content in the form of Video
+    :param post_id: post_id
+    :return: type_of_post (Integer)
+    """
+    try:
+        conn = create_connection()
+        cur = conn.cursor()
+        cur.execute(f"""
+                        SELECT type_of_post FROM posts
+                        WHERE post_id = ?;""", (post_id, ))
+        row = cur.fetchone()
+        conn.commit()
+        conn.close()
+        if row:
+            return row[0]
+        else:
+            return None
+    except Error as e:
+        print(e)
+        conn.rollback()
+
+
+# Checking Functions:
+
+
+def check_if_post_exists(post_id):
+    """ checks if a post already exists
+    :param post_id
+    :return: post_id (if the post exists) / None if not
+    """
+    try:
+        conn = create_connection()
+        cur = conn.cursor()
+        cur.execute(f"""
+                        SELECT post_id FROM posts
+                        WHERE  post_id = ?;""", (post_id, ))
+        row = cur.fetchone()
+        conn.commit()
+        conn.close()
+        if row:
+            return row[0]
+        else:
+            return None
+    except Error as e:
+        print(e)
+        conn.rollback()
+
+
+
+
+
+
